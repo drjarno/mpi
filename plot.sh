93aca02038ac028f64b8d566f7477e4900f52bc3
Jarno van der Kolk <jvanderk@uottawa.ca>
2018-11-05 14:06:28 -0500

    Better plotting

--- plot.sh
+++ plot.sh
@@ -12,7 +12,7 @@ count=$((num_iterations / save_every - 1))
 mkdir -p frames
 for i in `seq 0 ${count}`
 do
-  gnuplot -e "set terminal pngcairo enhanced size 640,480; set output sprintf('frames/frame_%04d.png', $i); set title sprintf('Time: %.2f s', $dt * $i); set yrange[0:1.05*${spike_temperature}]; plot 'heat_${i}.dat' using (\$0*${dx}):1 with lines notitle"
+  gnuplot -e "set terminal pngcairo enhanced size 640,480; set output sprintf('frames/frame_%04d.png', $i); set title sprintf('Time: %.2f s', $dt * $i); set yrange[0:1.05*${spike_temperature}]; set xlabel 'Position (a.u.)'; set ylabel 'Temperature (a.u.)'; plot 'heat_${i}.dat' using (\$0*${dx}):1 with lines notitle"
 done
 
 ffmpeg -i frames/frame_%04d.png -r 12 animation.webm -y && rm -r frames/
